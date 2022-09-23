66ecda43238158121f46642d366a6edb63a39849
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-09-23 14:05:13 -0700

    Added View+Extensions and  organized UI folder into subfolders

---
+++ iAddThree/UI/Shared/View+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  View+Extension.swift
+//  iAddThree
+//
+//  Created by Nikolai Nobadi on 9/23/22.
+//
+
+import SwiftUI
+
+extension View {
+    var isPad: Bool { UIDevice.current.userInterfaceIdiom == .pad }
+    var isSmallDevice: Bool { screenHeight < 600 }
+    var screenWidth: CGFloat { UIScreen.main.bounds.size.width }
+    var screenHeight: CGFloat { UIScreen.main.bounds.size.height }
+    
+    /// Percent required in parameter is direct representation. Example: 1% of width = getWidthPercent(1). 10% of width = getWidthPercent(10)
+    func getWidthPercent(_ percent: CGFloat) -> CGFloat { screenWidth * (percent * 0.01) }
+    
+    /// Percent required in parameter is direct representation. Example: 1% of height = getHeightPercent(1). 10% of height = getHeightPercent(10)
+    func getHeightPercent(_ percent: CGFloat) -> CGFloat { screenHeight * (percent * 0.01) }
+}
