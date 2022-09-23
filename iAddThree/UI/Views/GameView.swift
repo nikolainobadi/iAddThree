9f1c29071591ef9609078fda92951b5bc013d870
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-09-23 14:28:16 -0700

    Added ChalkButton

--- iAddThree/UI/Views/GameView.swift
+++ iAddThree/UI/Views/GameView.swift
@@ -10,14 +10,43 @@ import SwiftUI
 struct GameView: View {
     @State var isPlaying = false
     
+    private func showInstructions() { }
+    private func startGame() {
+        withAnimation {
+            isPlaying = true
+        }
+    }
+    
     var body: some View {
         VStack {
             if isPlaying {
                 // playView
                 
             } else {
-                // menuButtons
+                ChalkButton("Start Game", action: startGame)
+                ChalkButton("How to Play", action: showInstructions)
             }
+        }
+    }
+}
+
+struct ChalkButton: View {
+    let text: String
+    let style: Font.TextStyle
+    let action: () -> Void
+    
+    init(_ text: String, style: Font.TextStyle = .title3, action: @escaping () -> Void) {
+        self.text = text
+        self.style = style
+        self.action = action
+    }
+    
+    var body: some View {
+        Button(action: action) {
+            Text(text)
+                .padding(.horizontal, getWidthPercent(10))
+                .lineLimit(1)
+                .setChalkFont(style, autoSize: true)
         }
     }
 }
