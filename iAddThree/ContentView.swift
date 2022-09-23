689298faffd4ed2550d8ce7e32fdd701137d6bc6
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-09-23 12:42:19 -0700

    Initial Commit

---
+++ iAddThree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iAddThree
+//
+//  Created by Nikolai Nobadi on 9/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iAddThree/iAddThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAddThreeApp.swift
+//  iAddThree
+//
+//  Created by Nikolai Nobadi on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct iAddThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
