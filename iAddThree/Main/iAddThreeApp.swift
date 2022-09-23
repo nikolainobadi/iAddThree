d9c140433ea7afe9f053cb359bb9fa22324e1861
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-09-23 13:02:56 -0700

    Added AppLauncher to Main in order to skip launching actual app when running unit tests

--- iAddThree/Main/iAddThreeApp.swift
+++ iAddThree/Main/iAddThreeApp.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 @main
+struct AppLauncher {
+    static func main() throws {
+        if NSClassFromString("XCTestCase") == nil {
+            iAddThreeApp.main()
+        } else {
+            TestApp.main()
+        }
+    }
+}
+
+
 struct iAddThreeApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup { Text("Running Unit Tests") }
     }
 }
