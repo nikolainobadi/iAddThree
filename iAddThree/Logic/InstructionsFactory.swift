c910b1d273fdfa7e6bbed31f67829fe33944cb10
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-09-24 13:12:26 -0700

    Moved Instructions DataMOdel and InstructionsFactory to their own files

---
+++ iAddThree/Logic/InstructionsFactory.swift
@@ -0,0 +1,83 @@
+//
+//  InstructionsFactory.swift
+//  iAddThree
+//
+//  Created by Nikolai Nobadi on 9/24/22.
+//
+
+import Foundation
+
+class BaseInstructionsFactory {
+    static let sampleNumberList: [NumberItemPresenter] = NumberItemPresenter.defaultList
+    
+    static func makeUserAnsers(_ one: String, _ two: String? = nil, _ three: String? = nil, _ four: String? = nil) -> [String?] {
+        [one, two, three, four]
+    }
+    
+    static func addUserAnswers(_ answers: [String?], to list: [NumberItemPresenter]) -> [NumberItemPresenter] {
+        zip(list, answers).map { item, userAnswer in
+            var updated = item
+            updated.userAnswer = userAnswer
+            return updated
+        }
+    }
+}
+
+// MARK: - AddInstructions
+final class AddInstructionsFactory: BaseInstructionsFactory {
+    private override init() { super.init() }
+
+    static func makeInstructions() -> [InstructionDetails] { [firstPage, secondPage, thirdPage] }
+    
+    private static var firstPage: InstructionDetails {
+        InstructionDetails(id: 0, sampleNumberList: sampleNumberList, details:
+        """
+        Four random numbers will appear at the beginning of each round.
+
+        Add '3' to each number, starting on the left and making your way to the right.
+        """
+        )
+    }
+
+    private static var secondPage: InstructionDetails {
+        InstructionDetails(id: 1, sampleNumberList: addUserAnswers(makeUserAnsers("8", "2"), to: sampleNumberList), details:
+        """
+        When you answer correctly, the correct number will appear in green.
+
+        However, if you are incorrect, the number will appear in red.
+        """
+        )
+    }
+
+    private static var thirdPage: InstructionDetails {
+        InstructionDetails(id: 2, sampleNumberList: addUserAnswers(makeUserAnsers("8", "2", "1", "0"), to: sampleNumberList), details:
+        """
+        If the correct answer should be greater than '9', simply drop the tenths place ('drop the one')
+
+        Examples:
+        7+3 = '10', answer is '0'
+        8+3 = '11', answer is '1'
+        9+3 = '12', answer is '2'
+        """
+        )
+    }
+}
+
+
+// MARK: - Dependencies
+extension NumberItem {
+    static var defaultList: [NumberItem] {
+        [
+            NumberItem(number: 5, answer: 8),
+            NumberItem(number: 0, answer: 3),
+            NumberItem(number: 8, answer: 1),
+            NumberItem(number: 7, answer: 0)
+        ]
+    }
+}
+
+extension NumberItemPresenter {
+    static var defaultList: [NumberItemPresenter] {
+        NumberItem.defaultList.map({ NumberItemPresenter($0) })
+    }
+}
---
+++ iAddThree/Models/InstructionsViewDataModel.swift
@@ -0,0 +1,57 @@
+//
+//  InstructionsViewDataModel.swift
+//  iAddThree
+//
+//  Created by Nikolai Nobadi on 9/24/22.
+//
+
+import Foundation
+
+final class InstructionsDataModel: ObservableObject {
+    @Published var currentPage = 0
+
+    private let mode: GameMode
+    
+    init(mode: GameMode) {
+        self.mode = mode
+    }
+}
+
+
+// MARK: - View Model
+extension InstructionsDataModel {
+    var modeTitle: String { mode.title }
+    var sampleList: [NumberItemPresenter] { instructionsList[currentPage].sampleNumberList }
+    var instructions: String { currentDetails.details }
+    var showHowToTitle: Bool { isFirstPage }
+    var showPreviousButton: Bool { hasMultiplePages && !isFirstPage }
+    var showNextButton: Bool { hasMultiplePages && !isLastPage }
+    
+    func turnPage(backwards: Bool) {
+        if backwards {
+            if !isFirstPage { currentPage -= 1 }
+        } else {
+            if !isLastPage { currentPage += 1 }
+        }
+    }
+}
+
+
+// MARK: - Private Helpers
+private extension InstructionsDataModel {
+    var instructionsList: [InstructionDetails] { mode.instructionsList }
+    var currentDetails: InstructionDetails { instructionsList[currentPage] }
+    var hasMultiplePages: Bool { instructionsList.count > 1 }
+    var isFirstPage: Bool { currentPage == 0 }
+    var isLastPage: Bool { hasMultiplePages && currentPage == instructionsList.count - 1 }
+}
+
+
+// MARK: - Dependencies
+extension GameMode {
+    var instructionsList: [InstructionDetails] {
+        switch self {
+        case .add: return AddInstructionsFactory.makeInstructions()
+        }
+    }
+}
