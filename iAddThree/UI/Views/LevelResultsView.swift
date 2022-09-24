a2e8b8e153a6293675ee03b265ae82beaa9ca9c9
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-09-23 18:07:43 -0700

    Moved LevelResultsDataModel to its own file

--- iAddThree/UI/Views/LevelResultsView.swift
+++ iAddThree/UI/Views/LevelResultsView.swift
@@ -84,34 +84,4 @@ struct LevelResultsView_Previews: PreviewProvider {
     }
 }
 
-final class LevelResultsDataModel: ObservableObject {
-    @Published var currentScore: Int
-    @Published var showingGameOver = false
-    
-    private let newScore: Int?
-    private let previousLevel: Int
-    
-    let playNextLevel: () -> Void
-    
-    init(currentScore: Int, newScore: Int?, previousLevel: Int, playNextLevel: @escaping () -> Void) {
-        self.currentScore = currentScore
-        self.newScore = newScore
-        self.previousLevel = previousLevel
-        self.playNextLevel = playNextLevel
-    }
-}
 
-
-extension LevelResultsDataModel {
-    var completedLevel: Bool { newScore != nil }
-    var titleText: String { "Level \(previousLevel) complete" }
-    var playAgainText: String { completedLevel ? "Level \(previousLevel + 1)" : "Try again?" }
-    
-    func updateScore() {
-        if let newScore = newScore {
-            currentScore = newScore
-        } else {
-            showingGameOver = true
-        }
-    }
-}
