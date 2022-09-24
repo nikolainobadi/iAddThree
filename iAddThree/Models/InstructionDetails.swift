2e606a95e0b9c9bdf2dc9b70dd587b4c63fb166f
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-09-24 12:30:15 -0700

    Added InstructionDetails model

---
+++ iAddThree/Models/InstructionDetails.swift
@@ -0,0 +1,14 @@
+//
+//  InstructionDetails.swift
+//  iAddThree
+//
+//  Created by Nikolai Nobadi on 9/24/22.
+//
+
+import Foundation
+
+struct InstructionDetails {
+    let id: Int
+    let sampleNumberList: [NumberItemPresenter]
+    let details: String
+}
