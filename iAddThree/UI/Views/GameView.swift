52534f06733622b78246b0bfc49df652007a5099
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-09-24 12:27:52 -0700

    Added animations to GameView

--- iAddThree/UI/Views/GameView.swift
+++ iAddThree/UI/Views/GameView.swift
@@ -10,22 +10,57 @@ import SwiftUI
 struct GameView: View {
     @StateObject var dataModel: GameViewDataModel
     
-    private func showInstructions() { }
+    private var isPlaying: Bool { dataModel.isPlaying }
     
-    private func finishLevel(_ pointsToAdd: Int) {
-        
-    }
+    private func showInstructions() { }
+    private func finishLevel(_ pointsToAdd: Int) {}
     
     var body: some View {
         VStack {
-            if dataModel.isPlaying {
-                GameViewComposer.makePlayView(.add, finished: finishLevel(_:))
-                    .transition(.scale)
-            } else {
-                ChalkButton("Start Game", style: .title2, action: dataModel.startGame)
-                ChalkButton("How to Play", style: .subheadline, action: showInstructions)
-            }
-        }.animation(.default, value: dataModel.isPlaying)
+            GameTitle(title: dataModel.modeTitle, isPlaying: isPlaying)
+                
+            VStack {
+                Spacer()
+                if isPlaying {
+                    GameViewComposer.makePlayView(.add, finished: finishLevel(_:))
+                        .transition(.scale)
+                } else {
+                    MenuButtons(isPlaying: isPlaying, startGame: dataModel.startGame, showInstructions: showInstructions)
+                }
+                Spacer()
+            }.animation(.easeInOut(duration: 0.75), value: isPlaying)
+        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+    }
+}
+
+fileprivate struct GameTitle: View {
+    let title: String
+    let isPlaying: Bool
+    
+    var body: some View {
+        VStack {
+            Text(title).setChalkFont(.largeTitle, autoSize: true)
+                .scaleEffect(isPlaying ? 0.75 : 1)
+                .offset(y: isPlaying ? 0 : getHeightPercent(6))
+        }
+        .lineLimit(1)
+        .padding()
+        .animation(.linear(duration: 0.75), value: isPlaying)
+    }
+}
+
+fileprivate struct MenuButtons: View {
+    let isPlaying: Bool
+    let startGame: () -> Void
+    let showInstructions: () -> Void
+
+    var body: some View {
+        VStack {
+            ChalkButton("Start Game", style: .title2, action: startGame)
+            ChalkButton("How to Play", style: .subheadline, action: showInstructions)
+        }
+        .transition(.scale)
+        .animation(.default, value: isPlaying)
     }
 }
 
@@ -52,20 +87,37 @@ struct ChalkButton: View {
 }
 
 struct GameView_Previews: PreviewProvider {
-    static var dataModel: GameViewDataModel {
-        GameViewDataModel()
+    static func makeDataModel(_ mode: GameMode = .add) -> GameViewDataModel {
+        GameViewDataModel(mode: mode)
     }
     static var previews: some View {
-        GameView(dataModel: dataModel).onChalkboard()
+        GameView(dataModel: makeDataModel()).onChalkboard()
     }
 }
 
 
 final class GameViewDataModel: ObservableObject {
     @Published var isPlaying = false
+    
+    private let mode: GameMode
+    
+    init(mode: GameMode) {
+        self.mode = mode
+    }
 }
 
 
 extension GameViewDataModel {
+    var modeTitle: String { mode.title }
+    
     func startGame() { isPlaying = true }
+}
+
+
+extension GameMode {
+    var title: String {
+        switch self {
+        case .add: return "Add Three"
+        }
+    }
 }
