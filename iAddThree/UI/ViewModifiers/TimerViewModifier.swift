a00525f0e53bc734c52cf803a6d9dec935502e90
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-09-26 19:05:21 -0700

    Added timerColor to TimeViewModifier

--- iAddThree/UI/ViewModifiers/TimerViewModifier.swift
+++ iAddThree/UI/ViewModifiers/TimerViewModifier.swift
@@ -10,16 +10,34 @@ import SwiftUI
 struct TimerViewModifier: ViewModifier {
     @Binding var isActive: Bool
     @State var timeRemaining: Float
+    private var startTime: Float
     
     let finished: () -> Void
     
     private let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
     private var timerText: String { String(format: "%.2f", timeRemaining) }
+    private var timerColor: Color {
+        let percentageRemaining = timeRemaining / startTime
+        
+        if percentageRemaining > 0.6 { return .primary }
+        if percentageRemaining < 0.25 { return .red }
+        
+        return .yellow
+    }
+    
+    init(isActive: Binding<Bool>, timeRemaining: Float, finished: @escaping () -> Void) {
+        _isActive = isActive
+        self.timeRemaining = timeRemaining
+        self.startTime = timeRemaining
+        self.finished = finished
+    }
     
     func body(content: Content) -> some View {
         VStack {
             if isActive {
                 Text(timerText)
+                    .setSmoothFont(.body, textColor: timerColor)
+                    .padding(10)
             }
             content
         }.onReceive(timer) { _ in
