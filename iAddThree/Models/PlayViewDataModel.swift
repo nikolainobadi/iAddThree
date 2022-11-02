e2aa223d232d1e3f39d5057fb3f46ad5cc7e40dd
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-11-02 16:50:49 -0700

    Added finishLevel method to PlayViewDataModel (along with results & error publishers)

--- iAddThree/Models/PlayViewDataModel.swift
+++ iAddThree/Models/PlayViewDataModel.swift
@@ -5,22 +5,27 @@
 //  Created by Nikolai Nobadi on 11/1/22.
 //
 
+import Combine
 import Foundation
 
 final class PlayViewDataModel: ObservableObject {
+    @Published var error: Error?
+    @Published var results: LevelResultInfo?
     @Published var numberList: [NumberItemPresenter]
     
     private let store: GameStore
-    private let showResults: (LevelResultInfo) -> Void
+    
+    private var changes = Set<AnyCancellable>()
     
     init(numberList: [NumberItem], store: GameStore, showResults: @escaping (LevelResultInfo) -> Void) {
         self.store = store
-        self.showResults = showResults
         self.numberList = numberList.map({ NumberItemPresenter($0) })
+        self.$results
+            .compactMap { $0 }
+            .debounce(for: 1.5, scheduler: RunLoop.main)
+            .sink { showResults($0) }
+            .store(in: &changes)
     }
-    
-    // MARK: - TODO
-    // when allAnswersFilled || timerUp, should show resultBanner, then call showResults
 }
 
 
@@ -33,6 +38,27 @@ extension PlayViewDataModel {
     func submitNumber(_ number: String) {
         if let index = numberList.firstIndex(where: { $0.userAnswer == nil }) {
             numberList[index].userAnswer = number
+            
+            if allAnswersFilled {
+                finishLevel()
+            }
+        }
+    }
+}
+
+
+// MARK: - Private
+private extension PlayViewDataModel {
+    var pointsToAdd: Int { numberList.filter({ $0.isCorrect }).count }
+    var allAnswersFilled: Bool { !numberList.isEmpty && numberList.filter({ $0.userAnswer == nil }).isEmpty }
+    
+    func finishLevel() {
+        Task {
+            do {
+                results = try await store.loadResults(pointsToAdd: pointsToAdd)
+            } catch {
+                self.error = error
+            }
         }
     }
 }
