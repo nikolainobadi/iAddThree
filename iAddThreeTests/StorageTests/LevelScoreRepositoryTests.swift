920cc31bb57c0aff88e8dc07a910d167ea1c6da8
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-11-04 16:47:21 -0700

    Added unit tests for LevelScoreRepository

--- iAddThreeTests/StorageTests/LevelScoreRepositoryTests.swift
+++ iAddThreeTests/StorageTests/LevelScoreRepositoryTests.swift
@@ -9,14 +9,36 @@ import XCTest
 @testable import iAddThree
 
 final class LevelScoreRepositoryTests: XCTestCase {
+    func test_init_startingValuesEmpty() {
+        let sut = makeSUT()
+        
+        XCTAssertEqual(sut.score, 0)
+        XCTAssertEqual(sut.level, 1)
+    }
     
+    func test_updateLevel() {
+        let newLevel = 2
+        let sut = makeSUT()
+        
+        sut.updateLevel(newLevel)
+        
+        XCTAssertEqual(sut.level, newLevel)
+    }
     
+    func test_updateScore() {
+        let newScore = 4
+        let sut = makeSUT()
+        
+        sut.updateScore(newScore)
+        
+        XCTAssertEqual(sut.score, newScore)
+    }
 }
 
 
 // MARK: - SUT
 extension LevelScoreRepositoryTests {
-    func makeSUT(file: StaticString = #filePath, line: UInt = #line) {
-        
+    func makeSUT() -> LevelScoreRepository {
+        LevelScoreRepository()
     }
 }
