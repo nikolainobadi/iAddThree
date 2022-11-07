93b9cb0b9837deceade477384815187eb9ff03ea
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-11-07 11:02:17 -0800

    Added tests for LevelResultsDataModel

--- iAddThreeTests/DataModelTests/LevelResultsDataModelTests.swift
+++ iAddThreeTests/DataModelTests/LevelResultsDataModelTests.swift
@@ -1,52 +1,111 @@
-////
-////  LevelResultsDataModelTests.swift
-////  iAddThreeTests
-////
-////  Created by Nikolai Nobadi on 9/24/22.
-////
 //
-//import XCTest
-//@testable import iAddThree
+//  LevelResultsDataModelTests.swift
+//  iAddThreeTests
 //
-//final class LevelResultsDataModelTests: XCTestCase {
-//    func test_init_startingValuesEmpty() {
-//        let sut = makeSUT()
+//  Created by Nikolai Nobadi on 9/24/22.
 //
-//        XCTAssertEqual(sut.currentScore, 0)
-//        XCTAssertFalse(sut.showingGameOver)
-//    }
-//
-//    func test_completedLevel_noNewScore() {
-//        XCTAssertFalse(makeSUT().completedLevel)
-//    }
-//
-//    func test_completedLevel_withNewScore() {
-//        XCTAssertTrue(makeSUT(newScore: 4).completedLevel)
-//    }
-//
-//    func test_updateScore_noNewScore() {
-//        let sut = makeSUT()
-//
-//        sut.updateScore()
-//
-//        XCTAssertTrue(sut.showingGameOver)
-//    }
-//
-//    func test_updateScore_withNewScore() {
-//        let newScore = 4
-//        let sut = makeSUT(newScore: newScore)
-//
-//        sut.updateScore()
-//
-//        XCTAssertEqual(sut.currentScore, newScore)
-//    }
-//}
-//
-//
-//// MARK: - SUT
-//extension LevelResultsDataModelTests {
-//    func makeSUT(currentScore: Int = 0, newScore: Int? = nil, previousLevel: Int = 1, playNextLevel: @escaping () -> Void = { }, file: StaticString = #filePath, line: UInt = #line) -> LevelResultsDataModel {
-//
-//        LevelResultsDataModel(currentScore: currentScore, newScore: newScore, previousLevel: previousLevel, playNextLevel: playNextLevel)
-//    }
-//}
+
+import XCTest
+import Combine
+@testable import iAddThree
+
+final class LevelResultsDataModelTests: XCTestCase {
+    private var changes = Set<AnyCancellable>()
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        changes.removeAll()
+        XCTAssertTrue(changes.isEmpty)
+    }
+}
+
+
+// MARK: - Unit Tests
+extension LevelResultsDataModelTests {
+    func test_init_startingValues() {
+        let sut = makeSUT()
+        
+        XCTAssertEqual(sut.currentScore, 0)
+        XCTAssertFalse(sut.showingButton)
+        XCTAssertFalse(sut.showingViews)
+        XCTAssertEqual(sut.currentLevel, 1)
+        XCTAssertEqual(sut.nextLevel, 2)
+        XCTAssertNil(sut.newScore)
+    }
+    
+    func test_completedLevel_noPointsToAdd_returnsFalse() {
+        XCTAssertFalse(makeSUT().completedLevel)
+    }
+    
+    func test_completedLevel_withPointsToAdd_returnsTrue() {
+        XCTAssertTrue(makeSUT(results: makeResults(pointsToAdd: 1)).completedLevel)
+    }
+    
+    func test_playAgainText_noPointsToAdd() {
+        XCTAssertEqual(makeSUT().playAgainText, "Try Again?")
+    }
+    
+    func test_playAgainText_withPointsToAdd() {
+        XCTAssertNotEqual(makeSUT(results: makeResults(pointsToAdd: 1)).playAgainText, "Try Again?")
+    }
+    
+    func test_showResults() {
+        let sut = makeSUT()
+        
+        sut.showResults()
+        
+        XCTAssertTrue(sut.showingViews)
+    }
+    
+    func test_showResults_noPointsToAdd_triggersShowButton() {
+        let exp = expectation(description: "waiting for showButtons")
+        let sut = makeSUT()
+        
+        sut.$showingButton
+            .dropFirst()
+            .sink { XCTAssertTrue($0); exp.fulfill() }
+            .store(in: &changes)
+        
+        sut.showResults()
+        
+        waitForExpectations(timeout: 2.1)
+    }
+    
+    func test_showResults_withPointsToAdd_newScoreSet() {
+        let exp = expectation(description: "waiting for showButtons")
+        let currentScore = 0
+        let pointsToAdd = 4
+        let sut = makeSUT(results: makeResults(currentScore: currentScore, pointsToAdd: pointsToAdd))
+        
+        sut.$newScore
+            .drop(while: { $0 == nil })
+            .sink { XCTAssertEqual($0, pointsToAdd); exp.fulfill() }
+            .store(in: &changes)
+        
+        sut.showResults()
+        
+        waitForExpectations(timeout: 1.1)
+    }
+    
+    func test_playAgain() {
+        let exp = expectation(description: "waiting for playAgain")
+        let sut = makeSUT(playAgain: { exp.fulfill() })
+        
+        sut.playAgain()
+        
+        waitForExpectations(timeout: 0.1)
+    }
+}
+
+
+// MARK: - SUT
+extension LevelResultsDataModelTests {
+    func makeSUT(results: LevelResults? = nil, playAgain: @escaping () -> Void = { }) -> LevelResultsDataModel {
+        LevelResultsDataModel(results: results ?? makeResults(), playAgain: playAgain)
+    }
+    
+    func makeResults(currentScore: Int = 0, pointsToAdd: Int = 0, currentLevel: Int = 1, timerFinished: Bool = false) -> LevelResults {
+        LevelResults(currentScore: currentScore, pointsToAdd: pointsToAdd, currentLevel: currentLevel, timerFinished: timerFinished)
+    }
+}
