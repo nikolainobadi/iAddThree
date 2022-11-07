c357b06b5f9f2a01f89b57e2efcf510e46b7ea6f
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-11-07 11:04:36 -0800

    deleted OldTimerViewmodifier

--- iAddThree/UI/ViewModifiers/TimerViewModifier.swift
+++ iAddThree/UI/ViewModifiers/TimerViewModifier.swift
@@ -7,52 +7,6 @@
 
 import SwiftUI
 
-struct OldTimerViewModifier: ViewModifier {
-    @Binding var isActive: Bool
-    @State var timeRemaining: Float
-    private var startTime: Float
-    
-    let finished: () -> Void
-    
-    private let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
-    private var timerText: String { String(format: "%.2f", timeRemaining) }
-    private var timerColor: Color {
-        let percentageRemaining = timeRemaining / startTime
-        
-        if percentageRemaining > 0.6 { return .primary }
-        if percentageRemaining < 0.25 { return .red }
-        
-        return .yellow
-    }
-    
-    init(isActive: Binding<Bool>, timeRemaining: Float, finished: @escaping () -> Void) {
-        _isActive = isActive
-        self.timeRemaining = timeRemaining
-        self.startTime = timeRemaining
-        self.finished = finished
-    }
-    
-    func body(content: Content) -> some View {
-        VStack {
-            if isActive {
-                Text(timerText)
-                    .setSmoothFont(.body, textColor: timerColor)
-                    .padding(10)
-            }
-            content
-        }.onReceive(timer) { _ in
-            guard isActive else { return }
-            
-            if timeRemaining > 0 {
-                timeRemaining -= 0.05
-            } else {
-                isActive = false
-                finished()
-            }
-        }
-    }
-}
-
 struct TimerViewModifier: ViewModifier {
     @Binding var isActive: Bool
     @State var timeRemaining: Float
@@ -99,10 +53,6 @@ struct TimerViewModifier: ViewModifier {
 
 
 extension View {
-    func withTimer(isActive: Binding<Bool>, startTime: Float, finished: @escaping () -> Void) -> some View {
-        modifier(OldTimerViewModifier(isActive: isActive, timeRemaining: startTime, finished: finished))
-    }
-    
     func withTimer(isActive: Binding<Bool>, startTime: Float) -> some View {
         modifier(TimerViewModifier(isActive: isActive, timeRemaining: startTime))
     }
