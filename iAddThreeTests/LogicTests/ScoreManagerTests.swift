f4463d2af2c91c4b0efa3c3fd9c6d04679dab840
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-11-07 10:02:33 -0800

    ScoreManager fully tested

--- iAddThreeTests/LogicTests/ScoreManagerTests.swift
+++ iAddThreeTests/LogicTests/ScoreManagerTests.swift
@@ -16,7 +16,11 @@ final class ScoreManagerTests: XCTestCase {
         XCTAssertEqual(repo.score, 0)
         XCTAssertEqual(repo.level, 1)
     }
-    
+}
+
+
+// MARK: - Updater Tests
+extension ScoreManagerTests {
     func test_updateScore_noNewHighScore() async throws {
         let newScore = 4
         let highScore = 10
@@ -29,18 +33,6 @@ final class ScoreManagerTests: XCTestCase {
         XCTAssertEqual(store.highScore, highScore)
     }
     
-    func test_updateScore_noNewScore() async throws {
-//        let newScore = 0
-//        let highScore = 4
-//        let (sut, store, repo) = makeSUT(highScore: highScore)
-//
-//        try await sut.updateScore(newScore: newScore)
-//
-//        XCTAssertEqual(repo.level, 1)
-//        XCTAssertEqual(repo.score, 0)
-//        XCTAssertEqual(store.highScore, highScore)
-    }
-    
     func test_updateScore_newHighScore() async throws {
         let newScore = 4
         let highScore = 0
@@ -51,10 +43,6 @@ final class ScoreManagerTests: XCTestCase {
         XCTAssertEqual(repo.level, 2)
         XCTAssertEqual(repo.score, newScore)
         XCTAssertEqual(store.highScore, newScore)
-    }
-    
-    func test_updateScore_resetScore() async throws {
-        
     }
     
     func test_updateScore_error() async throws {
@@ -71,17 +59,58 @@ final class ScoreManagerTests: XCTestCase {
 }
 
 
+// MARK: - ResetHandler Tests
+extension ScoreManagerTests {
+    func test_resetHighScore() async throws {
+        let score = 10
+        let level = 5
+        let (sut, store, repo) = makeSUT(score: score, level: level, highScore: score)
+        
+        XCTAssertEqual(repo.level, level)
+        XCTAssertEqual(repo.score, score)
+        XCTAssertEqual(store.highScore, score)
+        
+        try await sut.resetHighScore()
+        
+        XCTAssertEqual(repo.level, 1)
+        XCTAssertEqual(repo.score, 0)
+        XCTAssertEqual(store.highScore, 0)
+    }
+    
+    func test_resetHighScore_error() async {
+        let (sut, _, _) = makeSUT(throwError: true)
+        
+        do {
+            try await sut.resetHighScore()
+            
+            XCTFail()
+        } catch {
+            XCTAssertTrue(true)
+        }
+    }
+}
+
+
 // MARK: - SUT
 extension ScoreManagerTests {
-    func makeSUT(highScore: Int = 0, throwError: Bool = false, file: StaticString = #filePath, line: UInt = #line) -> (sut: ScoreUpdater, highScoreStore: MockHighScoreStore, scoreRepo: LevelScoreStore) {
+    func makeSUT(score: Int = 0, level: Int = 1, highScore: Int = 0, throwError: Bool = false, file: StaticString = #filePath, line: UInt = #line) -> (sut: ScoreManager, highScoreStore: MockHighScoreStore, scoreRepo: LevelScoreStore) {
         
-        let repo = LevelScoreRepository()
+        let repo = makeScoreRepo(score: score, level: level)
         let store = MockHighScoreStore(highScore: highScore, throwError: throwError)
         let sut = ScoreManager(highScoreStore: store, levelScoreStore: repo)
         
         trackForMemoryLeaks(sut, file: file, line: line)
         
         return (sut, store, scoreRepo: repo)
+    }
+    
+    func makeScoreRepo(score: Int, level: Int) -> LevelScoreStore {
+        let repo = LevelScoreRepository()
+        
+        repo.score = score
+        repo.level = level
+        
+        return repo
     }
 }
 
