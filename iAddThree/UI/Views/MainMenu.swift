921f9209f3515e1914dcf883d3d547f8aee78bbc
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-11-14 17:44:13 -0800

    added empty MainMenu file

---
+++ iAddThree/UI/Views/MainMenu.swift
@@ -0,0 +1,20 @@
+//
+//  MainMenu.swift
+//  iAddThree
+//
+//  Created by Nikolai Nobadi on 11/14/22.
+//
+
+import SwiftUI
+
+struct MainMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMenu()
+    }
+}
