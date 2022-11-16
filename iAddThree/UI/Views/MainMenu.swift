6d291afdcfc2e897f2dc17876a7617f5a3e646cd
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-11-15 17:29:59 -0800

    updated title in MainMenu

--- iAddThree/UI/Views/MainMenu.swift
+++ iAddThree/UI/Views/MainMenu.swift
@@ -18,8 +18,7 @@ struct MainMenu: View {
     
     var body: some View {
         VStack {
-            Text("iAddThree")
-                .setChalkFont(.largeTitle)
+            AppTitleView(modeLevel: modeLevel)
             Spacer()
             ModeButtonsView(modes: availableModes, playMode: playMode(_:))
             Spacer()
@@ -27,6 +26,47 @@ struct MainMenu: View {
         .onChange(of: modeLevel, perform: { dataModel.updateModeLevel($0) })
         .fullScreenCover(item: $selectedMode) {
             GameView(mode: $0, dismiss: returnToMainMenu).onChalkboard()
+        }
+    }
+}
+
+
+// MARK: - Title
+fileprivate struct AppTitleView: View {
+    @State private var showingSubtractBanner = false
+    
+    let modeLevel: Int
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Text("i")
+                Text("Add").foregroundColor(.green)
+                Text("Three")
+            }
+            .lineLimit(1)
+            .setChalkFont(.largeTitle, autoSize: true)
+            .padding(.horizontal)
+            
+            if showingSubtractBanner {
+                HStack {
+                    Text("or Subtract")
+                        .setChalkFont(.body, textColor: .red)
+                        .padding(.horizontal)
+                    Spacer()
+                }
+                    .background(Color.black.opacity(0.4))
+                    .offset(x: 0, y: -getHeightPercent(1))
+                    .transition(.move(edge: .leading))
+            }
+        }.task {
+            try? await Task.sleep(nanoseconds: 1_000_000_000)
+            
+            if modeLevel > 1 {
+                withAnimation(.easeInOut(duration: 1)) {
+                    showingSubtractBanner = true
+                }
+            }
         }
     }
 }
