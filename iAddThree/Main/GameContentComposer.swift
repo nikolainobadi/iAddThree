93c1ec7e75fd2f27338fbf7fead753fd16728a86
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-11-15 18:10:49 -0800

    Added ModeLevelHighScoreStoreDecorator to Composer method (makePlayView)

--- iAddThree/Main/GameContentComposer.swift
+++ iAddThree/Main/GameContentComposer.swift
@@ -24,8 +24,9 @@ enum GameContentComposer {
     static func makePlayView(mode: GameMode, scoreStore: LevelScoreStore, showResults: @escaping (LevelResults) -> Void) -> some View {
         let numberList = NumberItemFactory.makeNumberList(mode)
         let highScoreStore = UserDefaultsHighScoreStore(mode: mode)
+        let highScoreDecorator = ModeLevelHighScoreStoreDecorator(mode: mode, decoratee: highScoreStore)
         let info = LevelInfo(score: scoreStore.score, level: scoreStore.level, highScore: highScoreStore.highScore)
-        let updater = ScoreManager(highScoreStore: highScoreStore, levelScoreStore: scoreStore)
+        let updater = ScoreManager(highScoreStore: highScoreDecorator, levelScoreStore: scoreStore)
         let dataModel = PlayViewDataModel(numberList: numberList, info: info, updater: updater, showResults: showResults)
         
         return PlayView(dataModel: dataModel)
