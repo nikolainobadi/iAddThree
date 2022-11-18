3289ea4522f1e1e1139b601e8e7212d37e6d080e
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-11-18 11:45:15 -0800

    Moved FinishedBannerMessageFactory to its own file Reformatted PlayView

---
+++ iAddThree/Logic/FinishedBannerMessageFactory.swift
@@ -0,0 +1,22 @@
+//
+//  FinishedBannerMessageFactory.swift
+//  iAddThree
+//
+//  Created by Nikolai Nobadi on 11/18/22.
+//
+
+import Foundation
+
+enum FinishedBannerMessageFactory {
+    static func makeMessage(_ results: LevelResults) -> String {
+        guard !results.timerFinished else { return "Times Up!" }
+        
+        switch results.pointsToAdd {
+        case 1: return "Nice Try"
+        case 2: return "Good Job!"
+        case 3: return "Gread Job!"
+        case 4: return "Perfect!"
+        default: return "Not Quite"
+        }
+    }
+}
--- iAddThree/UI/Views/PlayView.swift
+++ iAddThree/UI/Views/PlayView.swift
@@ -7,43 +7,9 @@
 
 import SwiftUI
 
-enum FinishedBannerMessageFactory {
-    static func makeMessage(_ results: LevelResults) -> String {
-        guard !results.timerFinished else { return "Times Up!" }
-        
-        switch results.pointsToAdd {
-        case 1: return "Nice Try"
-        case 2: return "Good Job!"
-        case 3: return "Gread Job!"
-        case 4: return "Perfect!"
-        default: return "Not Quite"
-        }
-    }
-}
-
 struct PlayView: View {
     @StateObject var dataModel: PlayViewDataModel
-    struct ChalkButton: View {
-        let text: String
-        let style: Font.TextStyle
-        let action: () -> Void
-        
-        init(_ text: String, style: Font.TextStyle = .title3, action: @escaping () -> Void) {
-            self.text = text
-            self.style = style
-            self.action = action
-        }
-        
-        var body: some View {
-            Button(action: action) {
-                Text(text)
-                    .padding(.horizontal, getWidthPercent(10))
-                    .lineLimit(1)
-                    .setChalkFont(style, textColor: Color(uiColor: .systemBackground), autoSize: true)
-                    .withRoundedBorder()
-            }
-        }
-    }
+
     private var score: Int { dataModel.score }
     private var highScore: Int { dataModel.highScore }
     private var results: LevelResults? { dataModel.results }
