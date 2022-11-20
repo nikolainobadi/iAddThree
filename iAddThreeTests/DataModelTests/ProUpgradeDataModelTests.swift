37a713433eb6c4fd3b0a081442be43725a70824a
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-11-20 15:55:14 -0800

    Added unit tests for fetchProduct in ProUpgradeDataModel

--- iAddThreeTests/DataModelTests/ProUpgradeDataModelTests.swift
+++ iAddThreeTests/DataModelTests/ProUpgradeDataModelTests.swift
@@ -9,17 +9,65 @@ import XCTest
 @testable import iAddThree
 
 final class ProUpgradeDataModelTests: XCTestCase {
+    func test_init_startingValuesEmpty() {
+        let (sut, store, defaults) = makeSUT()
+        
+        XCTAssertNil(sut.error)
+        XCTAssertTrue(sut.productName.isEmpty)
+        XCTAssertTrue(sut.productPrice.isEmpty)
+        XCTAssertTrue(store.name.isEmpty)
+        XCTAssertTrue(store.price.isEmpty)
+        XCTAssertNil(defaults.value(forKey: AppStorageKey.adsRemoved))
+    }
     
+    func test_fetchProduct() async throws {
+        let (sut, store, _) = makeSUT()
+        
+        await sut.fetchProduct()
+        try await waitForAsyncMethod()
+        
+        XCTAssertNil(sut.error)
+        XCTAssertFalse(store.name.isEmpty)
+        XCTAssertFalse(store.price.isEmpty)
+        XCTAssertFalse(sut.productName.isEmpty)
+        XCTAssertFalse(sut.productPrice.isEmpty)
+    }
     
+    func test_fetchProduct_error() async throws {
+        let (sut, store, _) = makeSUT(throwError: true)
+        
+        await sut.fetchProduct()
+        try await waitForAsyncMethod()
+        
+        XCTAssertNotNil(sut.error)
+        XCTAssertTrue(store.name.isEmpty)
+        XCTAssertTrue(store.price.isEmpty)
+        XCTAssertTrue(sut.productName.isEmpty)
+        XCTAssertTrue(sut.productPrice.isEmpty)
+    }
+
+//    func test_purchase() async {
+//
+//    }
+//
+//    func test_restorePurchases() async {
+//
+//    }
 }
 
 
 // MARK: - SUT
 extension ProUpgradeDataModelTests {
-//    func makeSUT(throwError: Bool = false, shouldPurchase: Bool = false, file: StaticString = #filePath, line: UInt = #line) -> (sut: ProUpgradeDataModel, store: MockStore) {
-//        let store = MockStore(throwError: throwError, shouldPurchase: shouldPurchase)
-//        let sut = ProUpgradeDataModel(store: store, defaults: <#T##UserDefaults#>)
-//    }
+    func makeSUT(throwError: Bool = false, shouldPurchase: Bool = false, file: StaticString = #filePath, line: UInt = #line) -> (sut: ProUpgradeDataModel, store: MockStore, defaults: UserDefaults) {
+        
+        let defaults = MockUserDefaults(suiteName: #file)!
+        let store = MockStore(throwError: throwError, shouldPurchase: shouldPurchase)
+        let sut = ProUpgradeDataModel(store: store, defaults: defaults)
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, store, defaults)
+    }
 }
 
 
