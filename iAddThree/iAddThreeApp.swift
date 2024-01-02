6e5e5739db95842bf9c838cc62da6c3df612e732
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-01-02 15:45:34 -0800

    Initial Commit

---
+++ iAddThree/iAddThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAddThreeApp.swift
+//  iAddThree
+//
+//  Created by Nikolai Nobadi on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct iAddThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
