790421f98ad40d18e7071abd32bc129bbe654c76
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-01-27 09:54:11 -0800

    fixed entitlements

--- iAddThree/Sources/GameCenter/SharedGameKitManager.swift
+++ iAddThree/Sources/GameCenter/SharedGameKitManager.swift
@@ -5,19 +5,19 @@
 //  Created by Nikolai Nobadi on 1/7/24.
 //
 
-import GameKit
+//import GameKit
 
 // MARK: - TODO -> still need to figure out how to implement
 enum SharedGameKitManager {
-    static var localPlayer: GKLocalPlayer {
-        return GKLocalPlayer.local
-    }
+//    static var localPlayer: GKLocalPlayer {
+//        return GKLocalPlayer.local
+//    }
 }
 
 
 // MARK: - Auth
 extension SharedGameKitManager {
-    static func authenticateLocalPlayer(presentingViewController: UIViewController?) {
+//    static func authenticateLocalPlayer(presentingViewController: UIViewController?) {
 //        guard let presentingViewController = presentingViewController else { return }
 //        
 //        localPlayer.authenticateHandler = { viewController, error in
@@ -25,70 +25,70 @@ extension SharedGameKitManager {
 //                presentingViewController.present(vc, animated: true, completion: nil)
 //            }
 //        }
-    }
+//    }
 }
 
 
 // MARK: - Leaderboard
 extension SharedGameKitManager {
-    static func loadHighScore(leaderboardId: String) async -> Int? {
-        guard localPlayer.isAuthenticated else { return nil }
-        
-        return nil
-    }
-    
-    static func saveHighScore(score: Int, leaderboardId: String) {
-        guard localPlayer.isAuthenticated else { return }
-        
-        Task {
-            try? await submitScore(score: score, leaderboardId: leaderboardId)
-        }
-    }
+//    static func loadHighScore(leaderboardId: String) async -> Int? {
+//        guard localPlayer.isAuthenticated else { return nil }
+//        
+//        return nil
+//    }
+//    
+//    static func saveHighScore(score: Int, leaderboardId: String) {
+//        guard localPlayer.isAuthenticated else { return }
+//        
+//        Task {
+//            try? await submitScore(score: score, leaderboardId: leaderboardId)
+//        }
+//    }
 }
 
 
 // MARK: - Achievements
 extension SharedGameKitManager {
-    static func loadAchievementIds() async -> [String] {
-        guard localPlayer.isAuthenticated else { return [] }
-        
-        let idList = try? await GKAchievement.loadAchievements().map({ $0.identifier })
-        
-        return idList ?? []
-    }
-    
-    static func reportAchievementList(idList: [String]) {
-        let achievements = idList.map({ makeGKAchievement(id: $0) })
-        
-        GKAchievement.report(achievements, withCompletionHandler: { error in
-            if let error = error {
-                print("Error reporting achievement: \(error.localizedDescription)")
-            }
-        })
-    }
+//    static func loadAchievementIds() async -> [String] {
+//        guard localPlayer.isAuthenticated else { return [] }
+//        
+//        let idList = try? await GKAchievement.loadAchievements().map({ $0.identifier })
+//        
+//        return idList ?? []
+//    }
+//    
+//    static func reportAchievementList(idList: [String]) {
+//        let achievements = idList.map({ makeGKAchievement(id: $0) })
+//        
+//        GKAchievement.report(achievements, withCompletionHandler: { error in
+//            if let error = error {
+//                print("Error reporting achievement: \(error.localizedDescription)")
+//            }
+//        })
+//    }
 }
 
 
 // MARK: - Private Methods
 private extension SharedGameKitManager {
-    static func loadHighScore(_ leaderboardId: String) async throws -> Int? {
-        let leaderboards = try await GKLeaderboard.loadLeaderboards(IDs: [leaderboardId])
-        guard let leaderboard = leaderboards.first(where: { $0.baseLeaderboardID == leaderboardId }) else { return nil }
-        
-        let (localPlayerEntry, _, _) = try await leaderboard.loadEntries(for: .global, timeScope: .allTime, range: NSRange(location: 1, length: 1))
-        
-        return localPlayerEntry?.score
-    }
-    
-    static func submitScore(score: Int, leaderboardId: String) async throws {
-        try await GKLeaderboard.submitScore(score, context: 0, player: localPlayer, leaderboardIDs: [leaderboardId])
-    }
-    
-    static func makeGKAchievement(id: String) -> GKAchievement {
-        let achievement = GKAchievement(identifier: id)
-        achievement.percentComplete = 100
-        achievement.showsCompletionBanner = true
-        
-        return achievement
-    }
+//    static func loadHighScore(_ leaderboardId: String) async throws -> Int? {
+//        let leaderboards = try await GKLeaderboard.loadLeaderboards(IDs: [leaderboardId])
+//        guard let leaderboard = leaderboards.first(where: { $0.baseLeaderboardID == leaderboardId }) else { return nil }
+//        
+//        let (localPlayerEntry, _, _) = try await leaderboard.loadEntries(for: .global, timeScope: .allTime, range: NSRange(location: 1, length: 1))
+//        
+//        return localPlayerEntry?.score
+//    }
+//    
+//    static func submitScore(score: Int, leaderboardId: String) async throws {
+//        try await GKLeaderboard.submitScore(score, context: 0, player: localPlayer, leaderboardIDs: [leaderboardId])
+//    }
+//    
+//    static func makeGKAchievement(id: String) -> GKAchievement {
+//        let achievement = GKAchievement(identifier: id)
+//        achievement.percentComplete = 100
+//        achievement.showsCompletionBanner = true
+//        
+//        return achievement
+//    }
 }
--- iAddThree/Sources/MainFeatures/Game/Storage/GameKitSocialPerformanceStore.swift
+++ iAddThree/Sources/MainFeatures/Game/Storage/GameKitSocialPerformanceStore.swift
@@ -9,18 +9,20 @@ import iAddThreeCore
 
 final class GameKitSocialPerformanceStore: SocialPerformanceStore {
     func loadHighScore(modeId: String) async -> Int? {
-        return await SharedGameKitManager.loadHighScore(leaderboardId: modeId)
+        nil
+//        return await SharedGameKitManager.loadHighScore(leaderboardId: modeId)
     }
     
     func loadUnlockedAchievements() async -> [GameAchievement] {
-        return await SharedGameKitManager.loadAchievementIds().map({ .init(identifier: $0) })
+        []
+//        return await SharedGameKitManager.loadAchievementIds().map({ .init(identifier: $0) })
     }
     
     func saveHighScore(_ newHighScore: Int, modeId: String) {
-        SharedGameKitManager.saveHighScore(score: newHighScore, leaderboardId: modeId)
+//        SharedGameKitManager.saveHighScore(score: newHighScore, leaderboardId: modeId)
     }
     
     func saveAchievements(_ achievements: [GameAchievement]) {
-        SharedGameKitManager.reportAchievementList(idList: achievements.map({ $0.identifier }))
+//        SharedGameKitManager.reportAchievementList(idList: achievements.map({ $0.identifier }))
     }
 }
